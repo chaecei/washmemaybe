dc71b748c209ae074c371d2a81d3864dcf7f32ca
kaizencture <camaelabarro03@gmail.com>
2025-04-22 11:16:36 +0800

    Added the Dashboard

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+
 
 Route::get('/', function () {
     return view('welcome');
